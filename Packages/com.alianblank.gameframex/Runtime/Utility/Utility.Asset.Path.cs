ddec73a49eda43855bd4f0a93016053a2f41c6ad
AlianBlank <wangfj11@foxmail.com>
2024-03-24 15:20:03 +0800

    [修改]1. 修改资源路径获取

--- Packages/com.alianblank.gameframex/Runtime/Utility/Utility.Asset.Path.cs
+++ Packages/com.alianblank.gameframex/Runtime/Utility/Utility.Asset.Path.cs
@@ -7,8 +7,21 @@
         /// </summary>
         public static class Asset
         {
+            /// <summary>
+            /// 路径
+            /// </summary>
             public static class Path
             {
+                /// <summary>
+                /// 打包资源根路径
+                /// </summary>
+                public const string BundlesPath = "Assets/Bundles";
+
+                /// <summary>
+                /// 打包资源文件夹名称
+                /// </summary>
+                public const string BundlesDirectoryName = "Bundles";
+
                 /// <summary>
                 /// 获取配置文件路径
                 /// </summary>
@@ -17,7 +30,18 @@
                 /// <returns></returns>
                 public static string GetConfigPath(string fileName, string extension = ".bytes")
                 {
-                    return $"Assets/Bundles/Config/{fileName}{extension}";
+                    return $"{BundlesPath}/Config/{fileName}{extension}";
+                }
+
+                /// <summary>
+                /// 获取AOT元数据代码文件路径
+                /// </summary>
+                /// <param name="fileName"></param>
+                /// <param name="extension"></param>
+                /// <returns></returns>
+                public static string GetAOTCodePath(string fileName, string extension = ".bytes")
+                {
+                    return $"{BundlesPath}/AOTCode/{fileName}{extension}";
                 }
 
                 /// <summary>
@@ -28,7 +52,7 @@
                 /// <returns></returns>
                 public static string GetCodePath(string fileName, string extension = ".bytes")
                 {
-                    return $"Assets/Bundles/Code/{fileName}{extension}";
+                    return $"{BundlesPath}/Code/{fileName}{extension}";
                 }
 
                 /// <summary>
@@ -39,7 +63,7 @@
                 /// <returns></returns>
                 public static string GetUIPackagePath(string uiPackageName, string extension = ".bytes")
                 {
-                    return $"Assets/Bundles/UI/{uiPackageName}/{uiPackageName}";
+                    return $"{BundlesPath}/UI/{uiPackageName}/{uiPackageName}";
                 }
             }
         }

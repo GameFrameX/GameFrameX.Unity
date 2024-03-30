86c93beb0a7483f35a5cf51a7bc883bd8f57227d
AlianBlank <wangfj11@foxmail.com>
2024-03-30 14:11:54 +0800

    [增加]1. 增加协程函数

--- Packages/com.alianblank.gameframex/Runtime/Coroutine/CoroutineComponent.cs
+++ Packages/com.alianblank.gameframex/Runtime/Coroutine/CoroutineComponent.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using System.Collections.Concurrent;
 using UnityEngine;
 
 namespace GameFrameX.Runtime
@@ -10,6 +11,69 @@ namespace GameFrameX.Runtime
     [AddComponentMenu("Game Framework/Coroutine")]
     public class CoroutineComponent : GameFrameworkComponent
     {
+        /// <summary>
+        /// 执行过的迭代器
+        /// </summary>
+        private readonly ConcurrentDictionary<IEnumerator, Coroutine> m_CoroutineMap = new ConcurrentDictionary<IEnumerator, Coroutine>();
+
+        /// <summary>
+        /// 开启一个协程
+        /// </summary>
+        /// <param name="enumerator"></param>
+        /// <returns></returns>
+        public new Coroutine StartCoroutine(IEnumerator enumerator)
+        {
+            var ret = base.StartCoroutine(enumerator);
+            m_CoroutineMap[enumerator] = ret;
+            return ret;
+        }
+
+        /// <summary>
+        /// 终止一个协程
+        /// </summary>
+        /// <param name="enumerator"></param>
+        public new void StopCoroutine(IEnumerator enumerator)
+        {
+            if (m_CoroutineMap.TryGetValue(enumerator, out var coroutine))
+            {
+                base.StopCoroutine(coroutine);
+                m_CoroutineMap.TryRemove(enumerator, out _);
+            }
+
+            base.StopCoroutine(enumerator);
+        }
+
+        /// <summary>
+        /// 终止一个协程
+        /// </summary>
+        /// <param name="coroutine"></param>
+        public new void StopCoroutine(Coroutine coroutine)
+        {
+            base.StopCoroutine(coroutine);
+            foreach (var item in m_CoroutineMap)
+            {
+                if (item.Value == coroutine)
+                {
+                    m_CoroutineMap.TryRemove(item.Key, out _);
+                    break;
+                }
+            }
+        }
+
+        /// <summary>
+        /// 终止全部协程
+        /// </summary>
+        public new void StopAllCoroutines()
+        {
+            foreach (var coroutine in m_CoroutineMap.Values)
+            {
+                base.StopCoroutine(coroutine);
+            }
+
+            m_CoroutineMap.Clear();
+            base.StopAllCoroutines();
+        }
+
         private readonly WaitForEndOfFrame _waitForEndOfFrame = new WaitForEndOfFrame();
 
         /// <summary>

b536c4caf8920aa8f079e6a2a5f816764144ebaf
Blank <wangfj11@foxmail.com>
2024-04-09 00:55:07 +0800

    [禁用]1. 禁用AOT帮助类的生成

--- Packages/com.alianblank.gameframex/Editor/CroppingHelper.cs
+++ Packages/com.alianblank.gameframex/Editor/CroppingHelper.cs
@@ -8,24 +8,24 @@ namespace GameFrameX.Editor
 {
     public static class CroppingHelper
     {
-        [MenuItem("Game Framework/CroppingHelper")]
-        static void Run()
-        {
-            var types = typeof(GameApp).Assembly.GetTypes();
-            types = types.OrderBy(m => m.FullName).ToArray();
-            StringBuilder stringBuilder = new StringBuilder();
-            foreach (var type in types)
-            {
-                if (type.IsNestedPrivate)
-                {
-                    continue;
-                }
-
-                stringBuilder.AppendLine(" _ = typeof(" + type.FullName.Replace("+", ".") + ");");
-            }
-
-            Debug.Log(stringBuilder);
-            File.WriteAllText(".\\CroppingHelper.txt", stringBuilder.ToString());
-        }
+        // [MenuItem("Game Framework/CroppingHelper")]
+        // static void Run()
+        // {
+        //     var types = typeof(GameApp).Assembly.GetTypes();
+        //     types = types.OrderBy(m => m.FullName).ToArray();
+        //     StringBuilder stringBuilder = new StringBuilder();
+        //     foreach (var type in types)
+        //     {
+        //         if (type.IsNestedPrivate)
+        //         {
+        //             continue;
+        //         }
+        //
+        //         stringBuilder.AppendLine(" _ = typeof(" + type.FullName.Replace("+", ".") + ");");
+        //     }
+        //
+        //     Debug.Log(stringBuilder);
+        //     File.WriteAllText(".\\CroppingHelper.txt", stringBuilder.ToString());
+        // }
     }
 }

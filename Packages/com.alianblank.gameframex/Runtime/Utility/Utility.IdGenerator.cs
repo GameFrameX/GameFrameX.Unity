ef2e42332d40f67ba09d34ca745df1e0a336c01d
AlianBlank <wangfj11@foxmail.com>
2024-05-11 13:51:25 +0800

    [增加]1. 增加唯一ID生成工具类

---
+++ Packages/com.alianblank.gameframex/Runtime/Utility/Utility.IdGenerator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+
+namespace GameFrameX
+{
+    public static partial class Utility
+    {
+        public static class IdGenerator
+        {
+            private static readonly DateTime s_UtcTimeStart = new DateTime(2022, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+
+            // 共享计数器
+            private static long _counter = (long)(DateTime.UtcNow - s_UtcTimeStart).TotalSeconds;
+
+            /// <summary>
+            /// 使用Interlocked.Increment生成唯一ID的方法
+            /// </summary>
+            /// <returns></returns>
+            public static long GetNextUniqueId()
+            {
+                // 原子性地递增值
+                return Interlocked.Increment(ref _counter);
+            }
+        }
+    }
+}

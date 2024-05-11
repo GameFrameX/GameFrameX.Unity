e10ba42665690f8ccd647714a48978603a4f6a1c
AlianBlank <wangfj11@foxmail.com>
2024-05-11 15:07:39 +0800

    [修改]1. 修改链接端口

--- Assets/Hotfix/HotfixLauncher.cs
+++ Assets/Hotfix/HotfixLauncher.cs
@@ -20,7 +20,7 @@ namespace Hotfix
     public static class HotfixLauncher
     {
         public static string serverIp = "127.0.0.1";
-        public static int serverPort = 21100;
+        public static int serverPort = 21000;
         private static INetworkChannel networkChannel;
 
         public static void Main()

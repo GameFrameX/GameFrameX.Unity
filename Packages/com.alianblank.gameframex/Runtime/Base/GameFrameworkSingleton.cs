93677cc0901ea3d3db21e98a54e629944264aa8e
AlianBlank <wangfj11@foxmail.com>
2024-05-12 19:21:24 +0800

    [增加]1. 增加框架单例基类

---
+++ Packages/com.alianblank.gameframex/Runtime/Base/GameFrameworkSingleton.cs
@@ -0,0 +1,31 @@
+﻿namespace Base
+{
+    /// <summary>
+    /// 游戏框架单例
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public abstract class GameFrameworkSingleton<T> where T : class, new()
+    {
+        private static T _instance;
+
+        protected GameFrameworkSingleton()
+        {
+        }
+
+        /// <summary>
+        /// 单例对象
+        /// </summary>
+        public static T Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new T();
+                }
+
+                return _instance;
+            }
+        }
+    }
+}

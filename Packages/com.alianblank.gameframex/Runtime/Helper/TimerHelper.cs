c8129a4d555ed954a410d3d66bf6948efa5c8aab
AlianBlank <wangfj11@foxmail.com>
2024-05-13 21:38:35 +0800

    [增加]1. 增加指定时间和Unix 时间戳的差的接口

--- Packages/com.alianblank.gameframex/Runtime/Helper/TimerHelper.cs
+++ Packages/com.alianblank.gameframex/Runtime/Helper/TimerHelper.cs
@@ -7,7 +7,8 @@ namespace GameFrameX.Runtime
     /// </summary>
     public static class GameTimeHelper
     {
-        private static readonly long Epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc).Ticks;
+        private static readonly DateTime EpochTime = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+        private static readonly long Epoch = EpochTime.Ticks;
 
         /// <summary>
         /// 时间差
@@ -16,12 +17,14 @@ namespace GameFrameX.Runtime
 
         private static bool _isSecLevel = true;
 
-        // public const long OneSecond = 1000;
-        // public const long OneSecond = 1000;
+        /// <summary>
+        /// 微秒
+        /// </summary>
         public const long TicksPerMicrosecond = 1; //100微秒
+
         public const long TicksPer = 10 * TicksPerMicrosecond;
-        public const long TicksPerMillisecond = TicksPer * 1000; //毫秒
-        public const long TicksPerSecond = TicksPerMillisecond * 1000; // 秒  //10000000
+        public const long TicksMillisecondUnit = TicksPer * 1000; //毫秒
+        public const long TicksSecondUnit = TicksMillisecondUnit * 1000; // 秒  //10000000
 
         /// <summary>
         /// 设置时间差
@@ -54,7 +57,7 @@ namespace GameFrameX.Runtime
         /// <returns></returns>
         public static long ClientNowMillisecond()
         {
-            return (DateTime.UtcNow.Ticks - Epoch) / TicksPerMillisecond;
+            return (DateTime.UtcNow.Ticks - Epoch) / TicksMillisecondUnit;
         }
 
         public static long ServerToday()
@@ -92,7 +95,7 @@ namespace GameFrameX.Runtime
         /// <returns></returns>
         public static long ClientToday()
         {
-            return (DateTime.Now.Date.ToUniversalTime().Ticks - Epoch) / TicksPerSecond;
+            return (DateTime.Now.Date.ToUniversalTime().Ticks - Epoch) / TicksSecondUnit;
         }
 
         /// <summary>
@@ -196,6 +199,28 @@ namespace GameFrameX.Runtime
         public static bool IsSameDay(DateTime time1, DateTime time2)
         {
             return time1.Date.Year == time2.Date.Year && time1.Date.Month == time2.Date.Month && time1.Date.Day == time2.Date.Day;
+        }
+
+        /// <summary>
+        /// 指定时间转换成Unix时间戳的时间差，单位秒
+        /// </summary>
+        /// <param name="time">指定时间</param>
+        /// <returns></returns>
+        public static long LocalTimeToUnixTimeSeconds(DateTime time)
+        {
+            var utcDateTime = time.ToUniversalTime();
+            return (long)(utcDateTime - EpochTime).TotalSeconds;
+        }
+
+        /// <summary>
+        /// 指定时间转换成Unix时间戳的时间差，单位毫秒
+        /// </summary>
+        /// <param name="time">指定时间</param>
+        /// <returns></returns>
+        public static long LocalTimeToUnixTimeMilliseconds(DateTime time)
+        {
+            var utcDateTime = time.ToUniversalTime();
+            return (long)(utcDateTime - EpochTime).TotalMilliseconds;
         }
     }
 }

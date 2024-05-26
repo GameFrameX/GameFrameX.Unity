14d3fe35c2684ca684ea64cd05a61bacdff1c357
Blank <wangfj11@foxmail.com>
2024-05-26 17:15:41 +0800

    [增加]1. 增加通知消息的处理方式示例

---
+++ Assets/Hotfix/Manager/BagManager.cs
@@ -0,0 +1,39 @@
+using Base;
+using GameFrameX.Network.Runtime;
+using GameFrameX.Runtime;
+using Hotfix.Proto;
+
+namespace Hotfix.Manager
+{
+    /// <summary>
+    /// 背包 管理器
+    /// </summary>
+    public sealed class BagManager : GameFrameworkSingleton<BagManager>, IMessageHandler
+    {
+        /// <summary>
+        /// 监听道具变化通知
+        /// </summary>
+        /// <param name="msg"></param>
+        [MessageHandler(typeof(RespItemChange), nameof(OnRespItemChange))]
+        private void OnRespItemChange(RespItemChange msg)
+        {
+            Log.Debug(msg);
+        }
+
+        /// <summary>
+        /// 由于是单例对象。所以在初始化的时候自动调用一次注册消息
+        /// </summary>
+        public BagManager()
+        {
+            Register();
+        }
+        
+        /// <summary>
+        /// 注册消息。请勿多次调用
+        /// </summary>
+        public void Register()
+        {
+            ProtoMessageHandler.Init(this);
+        }
+    }
+}

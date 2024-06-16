209fc6056cff193d24f827c6f19658b437d56118
Blank <wangfj11@foxmail.com>
2024-06-16 11:15:58 +0800

    [增加]1. 增加HTTP基础接口的参数适配

--- Assets/Scripts/Framework/Procedure/HttpHelper.cs
+++ Assets/Scripts/Framework/Procedure/HttpHelper.cs
@@ -5,22 +5,33 @@ using GameFrameX.Runtime;
 
 public static class HttpHelper
 {
+    private static readonly Dictionary<string, string> DictionaryParams = new Dictionary<string, string>();
+
     public static Dictionary<string, string> GetBaseParams()
     {
-        Dictionary<string, string> dictionary = new Dictionary<string, string>();
-        dictionary["Language"] = Application.systemLanguage.ToString();
-        dictionary["PackageName"] = Application.identifier;
-        dictionary["AppVersion"] = Application.version;
-        dictionary["Platform"] = PathHelper.GetPlatformName;
-        dictionary["Channel"] = BlankGetChannel.GetChannelName();
-        dictionary["SubChannel"] = BlankGetChannel.GetChannelName();
-
-        // if (GlobalConfigComponent.Instance.GlobalProto.DevelopmentMode)
-        // {
-        //     dictionary["Channel"] = "develop";
-        //     dictionary["SubChannel"] = "develop";
-        // }
-
-        return dictionary;
+        DictionaryParams["Language"] = Application.systemLanguage.ToString();
+        DictionaryParams["AppVersion"] = Application.version;
+#if UNITY_WEBGL
+        DictionaryParams["PackageName"] = "com.smartdog.bbqgame";
+#if ENABLE_WECHAT_MINI_GAME
+        DictionaryParams["Channel"] = "WxMiniGame";
+        DictionaryParams["SubChannel"] = "WxMiniGame";
+        DictionaryParams["Platform"] = "WebGLWxMiniGame";
+#elif ENABLE_DOUYIN_MINI_GAME
+        DictionaryParams["Channel"] = "DouYinMiniGame";
+        DictionaryParams["SubChannel"] = "DouYinMiniGame";
+        DictionaryParams["Platform"] = "WebGLDouYinMiniGame";
+#else
+        DictionaryParams["Channel"] = "WebGL";
+        DictionaryParams["SubChannel"] = "WebGL";
+        DictionaryParams["Platform"] = PathHelper.GetPlatformName;
+#endif
+#else
+        DictionaryParams["Platform"] = PathHelper.GetPlatformName;
+        DictionaryParams["PackageName"] = Application.identifier;
+        DictionaryParams["Channel"] = BlankGetChannel.GetChannelName();
+        DictionaryParams["SubChannel"] = BlankGetChannel.GetChannelName();
+#endif
+        return DictionaryParams;
     }
 }

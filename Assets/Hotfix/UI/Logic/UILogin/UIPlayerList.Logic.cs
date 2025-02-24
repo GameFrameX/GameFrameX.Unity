5bddf67940e5df870751a9265b4e78b04fdbadf8
Blank <wangfj11@foxmail.com>
2025-02-24 22:50:48 +0800

    [修复]1. 修复链接地址的适配

--- Assets/Hotfix/UI/Logic/UILogin/UIPlayerList.Logic.cs
+++ Assets/Hotfix/UI/Logic/UILogin/UIPlayerList.Logic.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Net;
 using FairyGUI;
 using GameFrameX.Event.Runtime;
@@ -74,7 +75,7 @@ namespace Hotfix.UI
             // 注册心跳消息
             DefaultPacketHeartBeatHandler packetSendHeaderHandler = new DefaultPacketHeartBeatHandler();
             networkChannel.RegisterHeartBeatHandler(packetSendHeaderHandler);
-            networkChannel.Connect(IPAddress.Parse(serverIp), serverPort);
+            networkChannel.Connect(new Uri("tcp://" + serverIp), serverPort);
             GameApp.Event.CheckSubscribe(NetworkConnectedEventArgs.EventId, OnNetworkConnected);
             GameApp.Event.CheckSubscribe(NetworkClosedEventArgs.EventId, OnNetworkClosed);
         }

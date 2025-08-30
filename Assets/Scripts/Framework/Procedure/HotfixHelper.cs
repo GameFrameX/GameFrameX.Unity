2390f5ee7e20dd209c75e639ed0567b5e538a88d
Blank <wangfj11@foxmail.com>
2025-08-30 19:16:32 +0800

    [增加]  HybridCLR的禁用适配

--- Assets/Scripts/Framework/Procedure/HotfixHelper.cs
+++ Assets/Scripts/Framework/Procedure/HotfixHelper.cs
@@ -1,7 +1,7 @@
-﻿using System;
+﻿using GameFrameX.Runtime;
+#if ENABLE_GAME_FRAME_X_HYBRID_CLR && !DISABLE_HYBRIDCLR
+using System;
 using System.Reflection;
-using GameFrameX.Runtime;
-#if ENABLE_GAME_FRAME_X_HYBRID_CLR
 using System.Linq;
 using HybridCLR;
 #endif
@@ -10,10 +10,16 @@ namespace Unity.Startup.Procedure
 {
     public static class HotfixHelper
     {
+#if (ENABLE_GAME_FRAME_X_HYBRID_CLR && !DISABLE_HYBRIDCLR)
         const string HotfixName = "Unity.Hotfix";
-
-        public static async void StartHotfix()
+#endif
+        public static
+#if ENABLE_GAME_FRAME_X_HYBRID_CLR && !DISABLE_HYBRIDCLR
+            async
+#endif
+            void StartHotfix()
         {
+#if ENABLE_GAME_FRAME_X_HYBRID_CLR && !DISABLE_HYBRIDCLR
             if (ApplicationHelper.IsEditor)
             {
                 var assemblies = Utility.Assembly.GetAssemblies();
@@ -29,7 +35,6 @@ namespace Unity.Startup.Procedure
                 return;
             }
 
-#if ENABLE_GAME_FRAME_X_HYBRID_CLR
             Log.Info("开始加载AOT DLL");
             var aotDlls = AOTGenericReferences.PatchedAOTAssemblyList.ToArray();
             foreach (var aotDll in aotDlls)
@@ -39,8 +44,8 @@ namespace Unity.Startup.Procedure
                 var aotBytes = assetHandle.GetAssetObject<UnityEngine.TextAsset>().bytes;
                 RuntimeApi.LoadMetadataForAOTAssembly(aotBytes, HomologousImageMode.SuperSet);
             }
+
             Log.Info("结束加载AOT DLL");
-#endif
             Log.Info("开始加载Unity.Hotfix.dll");
             var assetHotfixDllPath = Utility.Asset.Path.GetCodePath(HotfixName + Utility.Const.FileNameSuffix.DLL);
             var assetHotfixDllOperationHandle = await GameApp.Asset.LoadAssetAsync<UnityEngine.Object>(assetHotfixDllPath);
@@ -48,8 +53,12 @@ namespace Unity.Startup.Procedure
             Log.Info("开始加载程序集Hotfix");
             var hotfixAssembly = Assembly.Load(assemblyDataHotfixDll, null);
             Run(hotfixAssembly);
-        }
 
+#else
+            RunNative();
+#endif
+        }
+#if ENABLE_GAME_FRAME_X_HYBRID_CLR && !DISABLE_HYBRIDCLR
         private static void Run(Assembly assembly)
         {
             Log.Info("加载程序集Hotfix 结束 Assembly " + assembly.FullName);
@@ -59,5 +68,15 @@ namespace Unity.Startup.Procedure
             Log.Info("加载程序集Hotfix 结束 EntryType=>method " + method?.Name);
             method?.Invoke(null, null);
         }
+#else
+        private static void RunNative()
+        {
+            var entryType = Utility.Assembly.GetType("Hotfix.HotfixLauncher");
+            Log.Info("加载程序集Hotfix 结束 EntryType " + entryType.FullName);
+            var method = entryType.GetMethod("Main");
+            Log.Info("加载程序集Hotfix 结束 EntryType=>method " + method?.Name);
+            method?.Invoke(null, null);
+        }
+#endif
     }
 }

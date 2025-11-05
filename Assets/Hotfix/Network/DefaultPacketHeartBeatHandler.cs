ff7de88cd676f8009025825b31871d5bb2417fdd
Blank <wangfj11@foxmail.com>
2025-11-05 19:46:39 +0800

    [修复] 消息对象池的回收问题

--- Assets/Hotfix/Network/DefaultPacketHeartBeatHandler.cs
+++ Assets/Hotfix/Network/DefaultPacketHeartBeatHandler.cs
@@ -6,18 +6,14 @@ namespace Hotfix.Network
 {
     public sealed class DefaultPacketHeartBeatHandler : BasePacketHeartBeatHandler
     {
-        private readonly ReqHeartBeat _reqHeartBeat;
-
-        public DefaultPacketHeartBeatHandler()
-        {
-            _reqHeartBeat = new ReqHeartBeat();
-        }
-
         public override MessageObject Handler()
         {
-            _reqHeartBeat.Timestamp = TimerHelper.UnixTimeMilliseconds();
-            _reqHeartBeat.UpdateUniqueId();
-            return _reqHeartBeat;
+            var reqHeartBeat = new ReqHeartBeat
+            {
+                Timestamp = TimerHelper.UnixTimeMilliseconds(),
+            };
+            reqHeartBeat.UpdateUniqueId();
+            return reqHeartBeat;
         }
     }
 }
